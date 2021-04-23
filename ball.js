1d0797f97645a5b7a9cc3f17c8cb07a960c08bd1
Heath <43174742+heathguy@users.noreply.github.com>
2021-04-23 16:24:23 -0400

    Update ball.js

--- ball.js
+++ ball.js
@@ -5,8 +5,39 @@ class Ball {
     this.velocity = p5.Vector.random2D();
     this.velocity.mult(3);
     this.r = r;
-    this.m = r * 0.1;
+    //this.m = r * 0.1;
   }
+  
+  checkBoundaryCollision() {
+    if (this.position.x > width - this.r) {
+      this.position.x = width - this.r;
+      this.velocity.x *= -1;
+    } else if (this.position.x < this.r) {
+      this.position.x = this.r;
+      this.velocity.x *= -1;
+    } else if (this.position.y > height - this.r) {
+      this.position.y = height - this.r;
+      this.velocity.y *= -1;
+    } else if (this.position.y < this.r) {
+      this.position.y = this.r;
+      this.velocity.y *= -1;
+    }
+  }
+  
+  checkBrickCollision(brickList) {
+    for (let i = 0; i < brickList.length; i++) {
+      let b = brickList[i];
+
+      let hit = collideRectCircle(b.getX(), b.getY(), b.getW(), b.getH(), this.position.x, this.position.y, this.r);
+
+      //print('colliding?', hit);
+      if(hit) {
+        this.velocity.y *= -1;
+        b.hits--;
+        break;
+      } 
+    }
+}
   
   update() {
     this.position.add(this.velocity);
@@ -14,7 +45,7 @@ class Ball {
   
   display() {
     noStroke();
-    fill(204);
+    fill(240,240,240);
     ellipse(this.position.x, this.position.y, this.r * 2, this.r * 2);
   }
   
